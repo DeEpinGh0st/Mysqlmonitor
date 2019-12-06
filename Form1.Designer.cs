8148d4c2d2f93c775cd3ce8997e98446a8f6612a
DeEpinGh0st <1510536500@qq.com>
2019-12-06 17:16:39 +0800

    init commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace MysqlMonitor
+{
+    partial class Mysql_Monitor
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txt_searchkey = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Btn_start = new System.Windows.Forms.Button();
+            this.txt_pass = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_user = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_host = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Bs = new System.Windows.Forms.BindingSource(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.复制ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Bs)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txt_searchkey);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.Btn_start);
+            this.groupBox1.Controls.Add(this.txt_pass);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txt_user);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txt_host);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(867, 52);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "配置";
+            // 
+            // txt_searchkey
+            // 
+            this.txt_searchkey.Location = new System.Drawing.Point(660, 20);
+            this.txt_searchkey.Name = "txt_searchkey";
+            this.txt_searchkey.Size = new System.Drawing.Size(180, 21);
+            this.txt_searchkey.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(625, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "搜索";
+            // 
+            // Btn_start
+            // 
+            this.Btn_start.Location = new System.Drawing.Point(513, 19);
+            this.Btn_start.Name = "Btn_start";
+            this.Btn_start.Size = new System.Drawing.Size(75, 23);
+            this.Btn_start.TabIndex = 6;
+            this.Btn_start.Text = "监控";
+            this.Btn_start.UseVisualStyleBackColor = true;
+            this.Btn_start.Click += new System.EventHandler(this.Btn_start_Click);
+            // 
+            // txt_pass
+            // 
+            this.txt_pass.Location = new System.Drawing.Point(375, 20);
+            this.txt_pass.Name = "txt_pass";
+            this.txt_pass.Size = new System.Drawing.Size(121, 21);
+            this.txt_pass.TabIndex = 5;
+            this.txt_pass.Text = "root";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(340, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "密码";
+            // 
+            // txt_user
+            // 
+            this.txt_user.Location = new System.Drawing.Point(238, 20);
+            this.txt_user.Name = "txt_user";
+            this.txt_user.Size = new System.Drawing.Size(94, 21);
+            this.txt_user.TabIndex = 3;
+            this.txt_user.Text = "root";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(191, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "用户名";
+            // 
+            // txt_host
+            // 
+            this.txt_host.Location = new System.Drawing.Point(45, 20);
+            this.txt_host.Name = "txt_host";
+            this.txt_host.Size = new System.Drawing.Size(140, 21);
+            this.txt_host.TabIndex = 1;
+            this.txt_host.Text = "127.0.0.1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "主机";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 71);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(867, 367);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.复制ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(101, 26);
+            // 
+            // 复制ToolStripMenuItem
+            // 
+            this.复制ToolStripMenuItem.Name = "复制ToolStripMenuItem";
+            this.复制ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.复制ToolStripMenuItem.Text = "复制";
+            this.复制ToolStripMenuItem.Click += new System.EventHandler(this.复制ToolStripMenuItem_Click);
+            // 
+            // Mysql_Monitor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(892, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Mysql_Monitor";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mysql Monitor";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Bs)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txt_searchkey;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Btn_start;
+        private System.Windows.Forms.TextBox txt_pass;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_user;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_host;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource Bs;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 复制ToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace MysqlMonitor
+{
+    public partial class Mysql_Monitor : Form
+    {
+        public Mysql_Monitor()
+        {
+            InitializeComponent();
+        }
+
+        public MySqlConnection func_getmysqlcon()
+        {
+            string connStr = string.Concat(new string[]
+            {
+                "server=",
+                this.txt_host.Text,
+                ";user=",
+                this.txt_user.Text,
+                ";password=",
+                this.txt_pass.Text,
+                ";database=mysql"
+            });
+            return new MySqlConnection(connStr);
+        }
+
+        public int func_getmysqlcom(string M_str_sqlstr)
+        {
+            MySqlConnection mySqlConnection = this.func_getmysqlcon();
+            mySqlConnection.Open();
+            MySqlCommand mySqlCommand = new MySqlCommand(M_str_sqlstr, mySqlConnection);
+            int result = mySqlCommand.ExecuteNonQuery();
+            mySqlCommand.Dispose();
+            mySqlConnection.Close();
+            mySqlConnection.Dispose();
+            return result;
+        }
+
+        public DataSet func_getmysqlread(string M_str_sqlstr)
+        {
+            MySqlConnection mySqlConnection = this.func_getmysqlcon();
+            mySqlConnection.Open();
+            MySqlDataAdapter mySqlDataAdapter = new MySqlDataAdapter(M_str_sqlstr, mySqlConnection);
+            DataSet dataSet = new DataSet();
+            mySqlDataAdapter.Fill(dataSet);
+            return dataSet;
+        }
+
+        private void Btn_start_Click(object sender, EventArgs e)
+        {
+            string Str_Datetime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            //try
+            //{
+            this.func_getmysqlcom("set global general_log=on;SET GLOBAL log_output='table';");
+            string m_str_sqlstr = "select event_time,argument from mysql.general_log where command_type='Query' and argument not like 'set global general_log=on;SET GLOBAL log_output%' and argument not like 'select event_time,argument from%' and event_time > '" + Str_Datetime + "'";
+            DataSet dataSet = this.func_getmysqlread(m_str_sqlstr);
+            DataTableCollection tables = dataSet.Tables;
+            DataView dataSource = new DataView(tables[0]);
+            this.Bs = new BindingSource();
+            this.Bs.DataSource = dataSource;
+            this.dataGridView1.DataSource = this.Bs;
+            this.dataGridView1.Columns[0].HeaderText = "查询时间";
+            this.dataGridView1.Columns[1].HeaderText = "查询语句";
+            this.dataGridView1.Columns[0].Width = 150;
+            this.dataGridView1.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show(ex.Message);
+            //    //MessageBox.Show("数据库出错，请检查连接信息以及确认mysql版本在5.1.6以上", "提示");
+            //}
+        }
+
+        private void 复制ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this.dataGridView1.SelectedRows.Count > 0)
+            {
+                Clipboard.SetDataObject(this.dataGridView1.SelectedRows[0].Cells[1].Value.ToString());
+            }
+        }
+    }
+}
