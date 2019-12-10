ee519afc797ba41d7fa1392b273c9df0bd02a8fe
DeEpinGh0st <1510536500@qq.com>
2019-12-10 09:20:36 +0800

    添加图标

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Mysql_Monitor));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.Breakpoint_btn = new System.Windows.Forms.Button();
             this.Btn_start = new System.Windows.Forms.Button();
@@ -245,6 +246,7 @@
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(908, 516);
             this.MinimumSize = new System.Drawing.Size(908, 516);
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("MysqlMonitor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("S0cke3t")]
 [assembly: AssemblyProduct("MysqlMonitor")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
